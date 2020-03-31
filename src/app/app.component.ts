6757ca1737728234ee7f527014a2b69faa8ca839
D3K31Z3R <benjamin.belderbos@hotmail.com>
2020-03-31 22:10:58 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,91 @@
 import { Component } from '@angular/core';
+import { APIService } from './api.service';
+import { NgModule } from '@angular/core';
+
+import { Subscriber } from 'rxjs';
+
+interface User {
+  name: string;
+}
+interface Notes {
+  content: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'Notes-App';
+
+  title = 'NotesApp';
+  columns = ['Name', 'Note', 'Show Notes', 'Delete'];
+  columnsNotes = ['content'];
+  userList: Array<User>;
+  noteList: Array<Notes>;
+  service: APIService;
+  nameOnNote: string;
+  nameToAdd: string;
+  nameForNote: string;
+  note: string;
+  user: string;
+  message: string;
+  messageToAdd: string;
+  deleteUserMessage: string;
+  userDeleted = false;
+  noteAdded = false;
+  showNotes = false;
+  addUser = false;
+  messageObject;
+
+  constructor(apiService: APIService) {
+    this.service = apiService;
+    apiService.getUsers().subscribe((data: Array<User>) => {
+      console.log(data);
+      this.userList = data;
+    });
+  }
+
+  GetUsers = () => this.service.getUsers().subscribe((data: Array<User>) => {
+    console.log(data);
+    this.userList = data;
+  })
+
+  AddUser = () => {
+    this.service.AddUser(this.nameToAdd).subscribe((response) => {
+      console.log(response);
+      this.GetUsers();
+    });
+  }
+
+  DeleteUser = (name: string) => {
+    this.service.DeleteUser(name).subscribe((response) => {
+      this.showNotes = false;
+      this.noteAdded = false;
+      this.GetUsers();
+    });
+  }
+
+  AddNote = () => {
+    this.service.AddNote(this.nameForNote, this.note).subscribe((response) => {
+      this.noteAdded = true;
+      this.GetNotes(this.nameForNote);
+    });
+  }
+
+  CreateNote = (name: string) => {
+    this.noteAdded = true;
+    this.nameForNote = name;
+    this.showNotes = false;
+  }
+
+  GetNotes = (name: string) => {
+    this.service.GetNotes(name).subscribe((data: Array<Notes>) => {
+      this.noteList = data;
+      this.showNotes = true;
+      this.user = name;
+      this.noteAdded = false;
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+import {HttpClientModule} from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import { MaterialModule } from './material/material.module';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MaterialModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
